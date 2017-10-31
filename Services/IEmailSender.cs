46f84628bdc90b8893cdaf5492d32e87904a98e9
Bryn Beaudry <A00932262@ad.bcit.ca>
2017-10-31 13:10:32 -0700

    first commit

---
+++ Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SQLiteWeb.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
